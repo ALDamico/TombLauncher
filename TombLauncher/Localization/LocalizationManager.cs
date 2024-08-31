3d8c149f1ccc2232bc128d4972b12f2e6081abd8
Andrea Luciano Damico <luciano.damico@mbsengineering.it>
2024-08-31 19:38:35 +0200

    Loading localization at runtime

--- TombLauncher/Localization/LocalizationManager.cs
+++ TombLauncher/Localization/LocalizationManager.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
@@ -20,6 +22,7 @@ public class LocalizationManager
         _localizationRelativePath = "Localization";
         ChangeLanguage(_defaultCulture);
     }
+
     private CultureInfo _currentCulture;
     private string _localizationRelativePath;
     private CultureInfo _defaultCulture;
@@ -37,10 +40,29 @@ public class LocalizationManager
             _application.Resources.MergedDictionaries.Remove(currentTranslations);
         }
 
-        var resourceKey = $"avares://{_application.Name}/{_localizationRelativePath}/{cultureName}.axaml";
-        var resourceKeyDefault = $"avares://{_application.Name}/{_localizationRelativePath}/{_defaultCulture.Name}.axaml";
-        var rd = (ResourceDictionary)AvaloniaXamlLoader.Load(new Uri(resourceKey));
-        var rdDefault = (ResourceDictionary)AvaloniaXamlLoader.Load(new Uri(resourceKeyDefault));
+        
+
+
+        var resourceKey =
+            $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}/{_localizationRelativePath}/{cultureName}.axaml";
+
+        var resourceKeyDefault =
+            $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}/{_localizationRelativePath}/{_defaultCulture}.axaml";
+        var defaultKeyXaml = File.ReadAllText(resourceKeyDefault);
+        string xaml;
+
+        try
+        {
+            xaml = File.ReadAllText(
+                resourceKey);
+        }
+        catch (FileNotFoundException)
+        {
+            xaml = defaultKeyXaml;
+        }
+        
+        var rd = AvaloniaRuntimeXamlLoader.Parse<ResourceDictionary>(xaml);
+        var rdDefault = AvaloniaRuntimeXamlLoader.Parse<ResourceDictionary>(defaultKeyXaml);
         var missingKeys = rdDefault.Keys.Except(rd.Keys);
         var resultingDictionary = new ResourceDictionary();
         foreach (var key in missingKeys)
@@ -50,17 +72,24 @@ public class LocalizationManager
 
         foreach (var key in rd.Keys)
         {
-            resultingDictionary.Add(key, rd[key]);
+            resultingDictionary.TryAdd(key, rd[key]);
         }
-        
+
         _application.Resources.MergedDictionaries.Add(resultingDictionary);
         _localizedStrings = resultingDictionary;
     }
 
     public Dictionary<string, string> GetSubsetInvertedByPrefix(string prefix)
     {
-        return _localizedStrings.Where(ls => (ls.Key as string)?.StartsWith(prefix + "_") == true)
-            .ToDictionary(pair => pair.Value.ToString().ToLowerInvariant(), pair => pair.Key.ToString().Remove(prefix).ToLowerInvariant());
+        var elements = _localizedStrings.Where(ls => (ls.Key as string)?.StartsWith(prefix + "_") == true);
+        var dictionary = new Dictionary<string, string>();
+        foreach (var element in elements)
+        {
+            dictionary.TryAdd(element.Value.ToString().ToLowerInvariant(),
+                element.Key.ToString().Remove(prefix).ToLowerInvariant());
+        }
+
+        return dictionary;
     }
 
     public string GetLocalizedString(string key, params object[] parms)
@@ -69,6 +98,7 @@ public class LocalizationManager
         {
             parms = [];
         }
+
         return !_localizedStrings.TryGetValue(key, out var s) ? key : string.Format((string)s ?? key, parms);
     }
 
