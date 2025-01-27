fafd844711402183905a5935229e578b0eb7d9a6
Andrea Luciano Damico <aldamico90@gmail.com>
2025-01-27 10:32:05 +0100

    Initial work to edit backup delay in settings

--- TombLauncher.Controls/Infotip.axaml.cs
+++ TombLauncher.Controls/Infotip.axaml.cs
@@ -2,6 +2,8 @@
 using Avalonia.Controls;
 using Avalonia.Data;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Material.Icons;
 using Material.Icons.Avalonia;
 
 namespace TombLauncher.Controls;
@@ -25,9 +27,28 @@ public partial class Infotip : UserControl
         set => SetValue(ToolTipContentProperty, value);
     }
 
+    public MaterialIconKind Icon
+    {
+        get => GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+
     public static readonly StyledProperty<string> HeaderProperty =
         AvaloniaProperty.Register<Infotip, string>(nameof(Header), default, false, BindingMode.TwoWay);
 
     public static readonly StyledProperty<object> ToolTipContentProperty =
         AvaloniaProperty.Register<Infotip, object>(nameof(ToolTipContent), default, false, BindingMode.TwoWay);
+
+    public static readonly StyledProperty<MaterialIconKind> IconProperty =
+        AvaloniaProperty.Register<Infotip, MaterialIconKind>(nameof(Icon), MaterialIconKind.QuestionMarkCircle, false,
+            BindingMode.TwoWay);
+
+    public static readonly StyledProperty<IBrush> IconForegroundProperty = AvaloniaProperty.Register<Infotip, IBrush>(
+        nameof(IconForeground), default, false, BindingMode.Default);
+
+    public IBrush IconForeground
+    {
+        get => GetValue(IconForegroundProperty);
+        set => SetValue(IconForegroundProperty, value);
+    }
 }
