07846020e07c3136e5dd459b36406d76a7c9b40d
Andrea Luciano Damico <aldamico90@gmail.com>
2025-02-10 21:13:06 +0100

    Fixes GameEngine enum values
    Minor UI improvement for LaunchOptionsDialogView

--- TombLauncher.Contracts/Enums/GameEngine.cs
+++ TombLauncher.Contracts/Enums/GameEngine.cs
@@ -10,20 +10,20 @@ public enum GameEngine
     [Description("Tomb Raider")]
     TombRaider1 = 1,
     [Description("Tomb Raider II")]
-    TombRaider2 = 2,
+    TombRaider2 = 1 << 2,
     [Description("Tomb Raider III: Adventures of Lara Croft")]
-    TombRaider3 = 3,
+    TombRaider3 = 1 << 3,
     [Description("Tomb Raider: The Last Revelation")]
-    TombRaider4 = 4,
+    TombRaider4 = 1 << 4,
     [Description("Tomb Raider Chronicles")]
-    TombRaider5 = 5,
+    TombRaider5 = 1 << 5,
     [Description("TEN")]
-    Ten = 6,
+    Ten = 1 << 6,
     // Lost Artefacts engines
     [Description("TR1X")]
     Tr1x = 1 << 8 | TombRaider1,
     [Description("TR2X")]
-    Tr2x = 2 << 8 | TombRaider2,
+    Tr2x = 1 << 8 | TombRaider2,
     [Description("Tomb Raider (DOS)")]
     TombRaider1Dos = 1 << 9 | TombRaider1,
     [Description("TombATI")]
