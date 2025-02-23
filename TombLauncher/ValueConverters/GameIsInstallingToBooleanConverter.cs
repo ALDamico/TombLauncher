a072dbc2b2ef180e45241a1a320822f2e17720f9
Andrea Luciano Damico <aldamico90@gmail.com>
2025-02-23 22:08:42 +0100

    Adds GameSearchResultCardView component
    Fixes card background in light theme
    Fixes button visibility inside card

---
+++ TombLauncher/ValueConverters/GameIsInstallingToBooleanConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using TombLauncher.ViewModels;
+
+namespace TombLauncher.ValueConverters;
+
+public class GameIsInstallingToBooleanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is InstallProgressViewModel vm)
+        {
+            if (vm.InstallCompleted)
+                return false;
+            return vm.IsInstalling || vm.IsDownloading;
+        }
+
+        return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
