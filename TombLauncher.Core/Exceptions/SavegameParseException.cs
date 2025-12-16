820623e2ca9ac9f413540bb9363f2177e52e693a
Andrea Luciano Damico <aldamico90@gmail.com>
2025-12-17 00:41:50 +0100

    Fixes process crash with newer TR1X savegame versions

---
+++ TombLauncher.Core/Exceptions/SavegameParseException.cs
@@ -0,0 +1,9 @@
+﻿namespace TombLauncher.Core.Exceptions;
+
+public class SavegameParseException : Exception
+{
+    public SavegameParseException(Exception cause) : base("An error occurred while attempting to parse a savegame", cause)
+    {
+        
+    }
+}
